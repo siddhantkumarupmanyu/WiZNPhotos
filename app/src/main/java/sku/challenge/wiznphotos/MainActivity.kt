8423c5dcd460e810e15f34d512631d02eaea09ac
Siddhant Kumar <siddhantkumarupmanyu@gmail.com>
2021-12-16 11:18:46 +0530

    update scratchpad.txt

--- app/src/main/java/sku/challenge/wiznphotos/MainActivity.kt
+++ app/src/main/java/sku/challenge/wiznphotos/MainActivity.kt
@@ -1,7 +1,7 @@
 package sku.challenge.wiznphotos
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
